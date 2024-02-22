f302dee113a0367182aeac75bf15a9389dabd7d0
Matt Simerson <matt@tnpi.net>
2024-02-21 23:27:05 -0800

    util text fix

--- test/util.js
+++ test/util.js
@@ -4,13 +4,14 @@ const { describe, it } = require('node:test')
 const util = require('../lib/util')
 
 describe('util', function () {
-  describe('setEnv', function () {
-    it('sets process.env.NODE_ENV', async () => {
-      assert.equal(process.env.NODE_ENV, undefined)
-      util.setEnv()
-      assert.ok(process.env.NODE_ENV)
+  if (process.env.NODE_ENV === undefined) {
+    describe('setEnv', function () {
+      it('sets process.env.NODE_ENV', async () => {
+        util.setEnv()
+        assert.ok(process.env.NODE_ENV)
+      })
     })
-  })
+  }
 
   describe('meta', () => {
     it('returns the package version', () => {
