6cd22606692a29ec2d610bef001fdbe9abe1d7ae
Matt Simerson <matt@tnpi.net>
2024-02-27 23:23:02 -0800

    off with GH reporter

--- test.sh
+++ test.sh
@@ -6,12 +6,12 @@ $NODE test.js setup
 if [ "$1" = "watch" ]; then
 	$NODE --test --watch
 else
-	if [ -n "$GITHUB_WORKFLOW" ]; then
-		npm i --no-save node-test-github-reporter
-		$NODE --test --test-reporter=node-test-github-reporter
-	else
+	# if [ -n "$GITHUB_WORKFLOW" ]; then
+		# npm i --no-save node-test-github-reporter
+		# $NODE --test --test-reporter=node-test-github-reporter
+	# else
 		$NODE --test --test-reporter=spec
-	fi
+	# fi
 fi
 
 # npx mocha --exit --no-warnings=ExperimentalWarning lib/*.test.js routes/*.test.js
